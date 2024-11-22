8e038e3732a07acf3ca2057a0b4efa8af07f339d
celovieiras <celovieirads@gmail.com>
2024-11-21 21:31:48 -0300

    modificando arquivo de conexão do banco

--- projeto_integrador_vi/__init__.py
+++ projeto_integrador_vi/__init__.py
@@ -1,5 +1,9 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLACHEMY_DATABASE_URI'] = ""
+
+db = SQLAlchemy(app)
 
 from projeto_integrador_vi import routes
